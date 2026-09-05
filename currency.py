d488303c0c24cb0fd157fab98910464425085a06
Mammad <foroghimohamadreza@gmail.com>
2026-09-05 20:54:01 +0330

    made the currency exchange for (USD/CAD/EUR)

--- currency.py
+++ currency.py
@@ -1,18 +1,30 @@
-
 valid_currency = ["usd",'eur','cad']
-
 while True:
-
     user_input =(input("Enter the amount:"))
-    if user_input.isdigit():
+    if user_input.isdigit() and int(user_input) > 0:
         amount = int(user_input)
         source_currency = input('Source Currency (USD/EUR/CAD): ')
         if source_currency in valid_currency :
-            continue
+            target_currency = input('Target Currency (USD/EUR/CAD): ')
+            if target_currency in valid_currency :
+                if source_currency == target_currency:
+                    print(amount)
+                elif source_currency == 'usd' and target_currency == 'cad':
+                    print(amount * 1.38)
+                elif source_currency == 'usd' and target_currency == 'eur':
+                    print(amount * 0.86)
+                elif source_currency == 'cad' and target_currency == 'usd':
+                    print(amount * 0.72)
+                elif source_currency == 'cad' and  target_currency == 'eur':
+                    print(amount * 1.61)
+                elif source_currency == 'eur' and target_currency == 'usd':
+                    print(amount * 1.16)
+                elif source_currency == 'eur' and target_currency == 'cad':
+                    print(amount * 0.62)
+            else:
+                print('Invalid currency')
         else :
-            print('Invalid amount')
+            print('Invalid currency')
         
     else:
         print('Invalid amount')
-
-    
